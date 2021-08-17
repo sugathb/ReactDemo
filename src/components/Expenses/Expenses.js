e58418ffad11731619ce38d0bad91468da9969e5
sugathb <sugathb@gmail.com>
2021-08-18 00:41:06 +1000

    conditionally-output-expenses-content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,20 +16,26 @@ const Expense = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        date={expense.date}
+        amount={expense.amount}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={filteredYear}
         onExpensesFilterChange={expensesFilterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          amount={expense.amount}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
